75a9397f50b46c4d3a49df00afb7012c6b9e5419
tora0001 <tobe.2309@hotmail.com>
2023-09-19 11:58:40 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const endpoint = "https://team-goofy-musicbase.azurewebsites.net";
+
+window.addEventListener("load", start);
+
+async function start() {
+  const artists = await getArtists();
+  showArtists(artists);
+}
+
+async function getArtists() {
+  const response = await fetch(`${endpoint}/artists`);
+  const data = await response.json();
+  return data;
+}
+
+async function getAlbums() {
+  const response = await fetch(`${endpoint}/albums`);
+  const data = await response.json();
+  return data;
+}
+
+async function getSongs() {
+  const response = await fetch(`${endpoint}/songs`);
+  const data = await response.json();
+  return data;
+}
+
+function showArtist(artist) {
+  const html = /* html */ `
+    <article>
+        <img src="${artist.image}">
+        <p>${artist.name}</p>
+        <button id= "btn-update" >Update</button>
+        <button id= "btn-delete" >Delete</button>
+    </article>`;
+  document.querySelector("#artists").insertAdjacentHTML("beforeend", html);
+}
+
+function showArtists(artists) {
+  document.querySelector("#artists").innerHTML = "";
+
+  for (const artist of artists) {
+    showArtist(artist);
+  }
+}
